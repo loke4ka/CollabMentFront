cc68dd7bdd3c6cf7edeebbd7663951f671fe559d
loke4ka <nitr1248@gmail.com>
2024-03-11 23:39:17 +0500

    Change <a> to <link> TEST

--- src/pages/LoginRegister.tsx
+++ src/pages/LoginRegister.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent, useState } from "react";
 import styles from "./LoginRegister.module.css";
 import stylesAnimation from "../components/globalAnimations.module.css";
 import axios from "axios";
-import { Link, useHref } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const API_URL = 'https://loke4ka.pythonanywhere.com/api/login';
 
@@ -34,8 +34,8 @@ const LoginRegister: FunctionComponent = () => {
         console.log('Data saved to localStorage:', localStorage.getItem('username'), localStorage.getItem('userToken'));
 
         // window.location.href = '/profile' + `/${username}`;
-        return useHref('/profile' + `/${username}`);
-        
+        return <Link to={'/profile' + `/${username}`} />
+
       } else {
         console.log('Authentication failed');
       }
